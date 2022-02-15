e623cd8453d0e82c0779ec0b2f5ef20d113efd39
GitHub Action <action@github.com>
2022-02-15 11:41:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/avimoto/quickstart/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/avimoto/quickstart/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.avimoto.quickstart.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.avimoto.quickstart.MyBundle
 
 class MyApplicationService {
 
